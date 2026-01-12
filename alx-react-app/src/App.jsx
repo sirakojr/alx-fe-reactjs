12b3f2190d9aaab330274506c418edd76e8369bf
sirak <sirakotsegaye@gmail.com>
2026-01-12 10:36:37 -0800

    abrelohd

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,7 +29,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+        </p>
+        <WelcomeMessage />
     </>
   )
 }
