3547438aed1729542ea27b4ae9d524db1cfd20a1
sirak <sirakotsegaye@gmail.com>
2026-02-25 20:23:31 +0300

    react

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,21 +1,10 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
+import TodoList from "./components/TodoList";
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <TodoList />
+    </>
   );
 }
 
